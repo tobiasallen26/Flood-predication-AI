178023d2b5fa7a49e72d26074526709c4de366df
Tobias Allen <ta502@cam.ac.uk>
2024-02-24 09:44:40 +0000

    Started to implement pytorch

--- prediction.py
+++ prediction.py
@@ -1,37 +1,61 @@
-from neuralNetwork import neuralNetwork
-from read_data import read_past_rain_data, read_past_river_data
-import numpy as np
+import torch
+from torch import nn
+from read_data import read_past_river_data, read_past_rain_data
 
-def input_filter(input):
-    return np.transpose(input)
 
-def comparison_array_func(ans):
-    return np.transpose(ans)
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
 
-def get_training_data():
-    rain = read_past_rain_data()
+def arrange_data():
+    rain_data = read_past_rain_data()
     river_data = read_past_river_data()
+    river_data_avg = river_data["avg_levels"]
+    river_data_min = river_data["min_levels"]
+    river_data_max = river_data["max_levels"]
+    for i in range(len(rain_data)-15):
+        print(river_data_avg[i:i+14])
+        print(river_data_min[i:i+14])
+        print(river_data_max[i:i+14])
+        print(rain_data[i:i+14])
+        print()
+        print(river_data_avg[i+15])
+        print(river_data_min[i+15])
+        print(river_data_max[i+15])
+        print()
     
-    data = []
-    for i in range(len(rain)-15):
-        data.append(
-            [rain[i:i+14] 
-            + list(river_data["min_levels"][i:i+14]) 
-            + list(river_data["avg_levels"][i:i+14]) 
-            + list(river_data["max_levels"][i:i+14])
-            , 
-            [river_data["min_levels"][i+14],
-            river_data["avg_levels"][i+14],
-            river_data["max_levels"][i+14]
-            ]])
 
-    return data
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+        
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
 if __name__ == "__main__":
-    nn = neuralNetwork([56, 50, 50, 50, 50, 50, 3], input_filter, comparison_array_func)
-    training_data = get_training_data()
+    model = NeuralNetwork().to(device)
+    print(model)
     
-    nn.train(training_data)
-    nn.run(training_data[-1][0], training_data[-1][1])
-    print(nn.get_output())
+    X = torch.rand(1, 28, 28, device=device)
+    logits = model(X)
+    pred_probab = nn.Softmax(dim=1)(logits)
+    y_pred = pred_probab.argmax(1)
+    print(f"Predicted class: {y_pred}")
+    
+    arrange_data()
     
